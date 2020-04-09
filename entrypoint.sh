4491891029b37f186e2db2264cb3251cbcabfffb
a772bNsz <kathyhuinali@gmail.com>
2020-04-09 13:44:51 -0400

    testing entrypoint for mayapy

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-env | grep "$PWD"
-env | grep "$MAYA_LOCATION"
-#export MAYA_LOCATION="/usr/autodesk/maya/"
-#mayapy -m unittest discover -s workdir/tests -p "test_*.py" -v
+echo ">>>>> ENVIRONMENT VARIABLES"
+env
+
+echo ">>>>> MAYAPY"
+mayapy -m unittest discover -s workdir/tests -p "test_*.py" -v
